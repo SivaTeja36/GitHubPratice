ebdbf6a0ab8551b868f2385e94fda676b9f2a213
SivaTeja36 <134479590+SivaTeja36@users.noreply.github.com>
2025-11-12 00:27:14 +0530

    Cleared the repo

--- Backend/config.py
+++
@@ -1,3 +0,0 @@
-class Config:
-    SQLALCHEMY_DATABASE_URI = "sqlite:///scraper.db" #Tells Flask to connect to a database file named scraper.db.
-    SQLALCHEMY_TRACK_MODIFICATIONS = False # This tells the app not to keep track of things that doesn’t have to.
--- first non repeating.py
+++
@@ -1,9 +0,0 @@
-input = input("enter a word:")#madhumathi
-for i in input:#m#d
-    count=0
-    for j in input:#m#a#d#d
-        if i==j:#m=m
-            count+=1#count=2
-    if count==1:#count=1
-        print("the first non repeating charaacter is:",i)
-        break
